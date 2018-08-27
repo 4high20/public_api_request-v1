8458e67f433a9839126d1e0fd875728822c59c76
Marco <4high20@gmail.com>
2018-08-27 12:07:42 +0200

    stuck on modal toggle

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,31 @@ searchForm.setAttribute('action', '#');
 searchForm.setAttribute('method', 'get');
 document.querySelector('.search-container').appendChild(searchForm);
 
+//declare search dom variables
+
+const searchSubmit = document.querySelector('#search-submit');
+const searchInput = document.querySelector('#search-input')
+const gallery = document.querySelector('#gallery');
+
+//listen for search inputs
+searchInput.addEventListener('keyup', filterUsers);
+searchSubmit.addEventListener('click', filterUsers);
+
+//this function filters the showed users based on the search input
+function filterUsers(){
+  const cardDiv = document.querySelectorAll('.card');
+  const names = document.querySelectorAll('.card-name');
+  for(let i = 0; i < names.length; i++){
+    let name = names[i].textContent;
+    name.toLowerCase();
+    if(!name.includes(searchInput.value.toLowerCase())){
+      names[i].parentNode.parentNode.style.display = 'none';
+    } else {
+      names[i].parentNode.parentNode.style.display = '';
+    }
+  }
+}
+
 //this function removes the modal window
 function removeModal(){
   const modal = document.querySelector('.modal-container');
@@ -15,56 +40,41 @@ function removeModal(){
 function createUsers(users){
   for(let i = 0; i < 12; i++){
     const galleryItems = document.createElement('div');
-    galleryItems.innerHTML = `<div class="card-img-container"><img class="card-img" src="${users[i].image}" alt="profile picture"></div><div class="card-info-container"><h3 id="name" class="card-name cap">${users[i].firstName} ${users[i].lastName}</h3><p class="card-text">${users[i].email}</p><p class="card-text cap">${users[i].city}</p></div>`;
+    galleryItems.innerHTML = `<div class="card-img-container"><img class="card-img" src="${users[i].picture.large}" alt="profile picture"></div><div class="card-info-container"><h3 id="name" class="card-name cap">${users[i].name.first} ${users[i].name.last}</h3><p class="card-text">${users[i].email}</p><p class="card-text cap">${users[i].location.city}</p></div>`;
     galleryItems.classList.add('card');
     document.querySelector('#gallery').appendChild(galleryItems);
     galleryItems.addEventListener('click', (e) => {
       createModal(users[i]);
-      const closeModal = document.querySelector('#modal-close-btn');
-      closeModal.addEventListener('click', (e) => {
+      const btnPrev = document.querySelector('#modal-prev');
+      const btnNext = document.querySelector('#modal-next');
+      btnPrev.addEventListener('click', () => {
         removeModal();
-      });
+        createModal(users[i - 1]);
+      })
+      btnNext.addEventListener('click', () => {
+        removeModal();
+        createModal(users[i + 1]);
+      })
     })
   }
-}
-
-//this function create a user object that will be used to create every user card and modal window
-function getUserData(data){
-  let obj = {};
-  obj.image = data.results[0].picture.large;
-  obj.firstName = data.results[0].name.first;
-  obj.lastName = data.results[0].name.last;
-  obj.email = data.results[0].email;
-  obj.city = data.results[0].location.city;
-  obj.cell = data.results[0].cell;
-  obj.street = data.results[0].location.street;
-  obj.state = data.results[0].location.state;
-  obj.post = data.results[0].location.postcode;
-  obj.bday = data.results[0].dob.date.substring(0, 10);
-  return obj;
 }
 
 //this function creates the modal window
 function createModal(user){
   const modalDiv = document.createElement('div');
   modalDiv.classList.add('modal-container');
-  modalDiv.innerHTML = `<div class="modal"><button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button><div class="modal-info-container"><img class="modal-img" src="${user.image}" alt="profile picture"><h3 id="name" class="modal-name cap">${user.firstName} ${user.lastName}</h3><p class="modal-text">${user.email}</p><p class="modal-text cap">${user.city}</p><hr><p class="modal-text">${user.cell}</p><p class="modal-text">${user.street}, ${user.state}, ${user.post}</p><p class="modal-text">Birthday: ${user.bday}</p></div>`;
+  modalDiv.innerHTML = `<div class="modal"><button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button><div class="modal-info-container"><img class="modal-img" src="${user.picture.large}" alt="profile picture"><h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3><p class="modal-text">${user.email}</p><p class="modal-text cap">${user.location.city}</p><hr><p class="modal-text">${user.cell}</p><p class="modal-text">${user.location.street}, ${user.location.state}, ${user.location.postcode}</p><p class="modal-text">Birthday: ${user.dob.date.substring(0, 10)}</p></div><div class="modal-btn-container"><button type="button" id="modal-prev" class="modal-prev btn">Prev</button><button type="button" id="modal-next" class="modal-next btn">Next</button></div></div>`;
   document.querySelector('body').appendChild(modalDiv);
+  const closeModal = document.querySelector('#modal-close-btn');
+  closeModal.addEventListener('click', (e) => {
+    removeModal();
+  });
 }
 
 //API url
-const url = 'https://randomuser.me/api/';
+const url = 'https://randomuser.me/api/?results=12&nat=gb';
 
-//fetch a random user from randomuser api 12 times and stores it into the usersArray
-const usersArray = [];
-for(let i = 0; i < 12; i++){
-  usersArray.push(
-    fetch(url)
-      .then(response => response.json())
-      .then(data => getUserData(data))
-  )
-}
-
-//create every user cards only after the 12 fetch promises are resolved
-Promise.all(usersArray)
-  .then(data => createUsers(data));
+//fetch 12 random users
+fetch(url)
+  .then(response => response.json())
+  .then(data => createUsers(data.results));
