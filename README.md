86860a4774df4f94d0350c79fd7e68f86bd055b6
Marco <4high20@gmail.com>
2018-08-31 11:07:32 +0200

    exceeds requirements working

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # public_api_request-v1
+
+CSS changes:
+
+- changed font
+- changed background color
+- changed cards background color
+- changed H1 color
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
+'use strict';
+
+const body = document.querySelector('body');
+
 //create the search form
 const searchForm = document.createElement('form');
-searchForm.innerHTML = '<input type="search" id="search-input" class="search-input" placeholder="Search..."><input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">';
+searchForm.innerHTML = `
+  <input type="search" id="search-input" class="search-input" placeholder="Search...">
+  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">`;
 searchForm.setAttribute('action', '#');
 searchForm.setAttribute('method', 'get');
 document.querySelector('.search-container').appendChild(searchForm);
 
+//variable to store the array of users objects returned from the fetch api
 let usersList;
+
+//variable to store which card is clicked to display the modal and switch the modal
+let userIndex = 0;
 
 //declare search dom variables
 const searchSubmit = document.querySelector('#search-submit');
@@ -37,16 +47,21 @@ function removeModal(){
   modal.remove();
 }
 
-let userIndex = 0;
-
 //this function creates gallery items div with each user card inside and listens for clicks on cards to display the modal window
 function createUsers(users){
   for(let i = 0; i < 12; i++){
     const galleryItems = document.createElement('div');
-    galleryItems.innerHTML = `<div class="card-img-container"><img class="card-img" src="${users[i].picture.large}" alt="profile picture"></div><div class="card-info-container"><h3 id="name" class="card-name cap">${users[i].name.first} ${users[i].name.last}</h3><p class="card-text">${users[i].email}</p><p class="card-text cap">${users[i].location.city}</p></div>`;
+    galleryItems.innerHTML = `
+      <div class="card-img-container">
+        <img class="card-img" src="${users[i].picture.large}" alt="profile picture">
+      </div>
+      <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${users[i].name.first} ${users[i].name.last}</h3>
+        <p class="card-text">${users[i].email}</p>
+        <p class="card-text cap">${users[i].location.city}</p>
+      </div>`;
     galleryItems.classList.add('card');
     document.querySelector('#gallery').appendChild(galleryItems);
-    test = galleryItems;
     galleryItems.addEventListener('click', (e) => {
       userIndex = i;
       createModal(users[i]);
@@ -54,7 +69,9 @@ function createUsers(users){
   }
 }
 
-document.addEventListener('click', (e) => {
+//this listener listen for a click on the Prev and Next button inside the modal
+//and let the user loop through the users while on the modal window
+body.addEventListener('click', (e) => {
   if(e.target.textContent === 'Prev' && userIndex !== 0){
     removeModal();
     createModal(usersList[userIndex - 1]);
@@ -71,7 +88,23 @@ document.addEventListener('click', (e) => {
 function createModal(user){
   const modalDiv = document.createElement('div');
   modalDiv.classList.add('modal-container');
-  modalDiv.innerHTML = `<div class="modal"><button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button><div class="modal-info-container"><img class="modal-img" src="${user.picture.large}" alt="profile picture"><h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3><p class="modal-text">${user.email}</p><p class="modal-text cap">${user.location.city}</p><hr><p class="modal-text">${user.cell}</p><p class="modal-text">${user.location.street}, ${user.location.state}, ${user.location.postcode}</p><p class="modal-text">Birthday: ${user.dob.date.substring(0, 10)}</p></div><div class="modal-btn-container"><button type="button" id="modal-prev" class="modal-prev btn">Prev</button><button type="button" id="modal-next" class="modal-next btn">Next</button></div></div>`;
+  modalDiv.innerHTML = `
+    <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+        <p class="modal-text">${user.email}</p>
+        <p class="modal-text cap">${user.location.city}</p>
+        <hr>
+        <p class="modal-text">${user.cell}</p>
+        <p class="modal-text">${user.location.street}, ${user.location.state}, ${user.location.postcode}</p><p class="modal-text">Birthday: ${user.dob.date.substring(0, 10)}</p>
+      </div>
+      <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+      </div>
+    </div>`;
   document.querySelector('body').appendChild(modalDiv);
   const closeModal = document.querySelector('#modal-close-btn');
   closeModal.addEventListener('click', (e) => {
